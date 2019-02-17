23082dabbef8ac03b0ec8a44647927bf3cf979d0
Danny Pham <dpham1006@gmail.com>
2019-02-17 13:58:56 -0800

    Added dashboard
    Minor fixes to login screen. Added initial dashboard design.

--- Pham IBCS IA/src/mainUI/mainUI.java
+++ Pham IBCS IA/src/mainUI/mainUI.java
@@ -15,13 +15,21 @@ import javax.imageio.ImageIO;
 
 /**
  *
- * @author htvph
+ * @author Danny Pham
+ * mainUI is the login screen
  */
 public class mainUI extends javax.swing.JFrame {
+
+    
 
     /**
      * Creates new form mainUI
      */
+    // sets up username/password strings
+    
+    String username, password;
+
+    // main constructor
     public mainUI() throws IOException {
         initComponents();
         
@@ -32,6 +40,8 @@ public class mainUI extends javax.swing.JFrame {
         // puts form in center of screen
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        
+        
     }
 
     /**
@@ -54,12 +64,12 @@ public class mainUI extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jSeparator2 = new javax.swing.JSeparator();
         textFieldUserName = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        passwordField = new javax.swing.JPasswordField();
         jLabel1 = new javax.swing.JLabel();
         panelSignIn = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
+        labelSignIn = new javax.swing.JLabel();
         panelNewProfile = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
+        labelNewProfile = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("TargetGrade");
@@ -113,6 +123,7 @@ public class mainUI extends javax.swing.JFrame {
 
         labelExit.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         labelExit.setForeground(new java.awt.Color(0, 0, 51));
+        labelExit.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         labelExit.setText("X");
         labelExit.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         labelExit.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -126,7 +137,7 @@ public class mainUI extends javax.swing.JFrame {
                 labelExitMouseExited(evt);
             }
         });
-        panelExit.add(labelExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(9, 0, 30, 40));
+        panelExit.add(labelExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 40, 40));
 
         jPanel2.add(panelExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 0, 40, 40));
 
@@ -156,12 +167,12 @@ public class mainUI extends javax.swing.JFrame {
         });
         jPanel2.add(textFieldUserName, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 90, 220, -1));
 
-        jPasswordField1.setBackground(new java.awt.Color(0, 153, 153));
-        jPasswordField1.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jPasswordField1.setForeground(new java.awt.Color(255, 255, 255));
-        jPasswordField1.setBorder(null);
-        jPasswordField1.setEchoChar('\u2022');
-        jPanel2.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, 220, -1));
+        passwordField.setBackground(new java.awt.Color(0, 153, 153));
+        passwordField.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        passwordField.setForeground(new java.awt.Color(255, 255, 255));
+        passwordField.setBorder(null);
+        passwordField.setEchoChar('\u2022');
+        jPanel2.add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, 220, -1));
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
@@ -170,6 +181,9 @@ public class mainUI extends javax.swing.JFrame {
 
         panelSignIn.setBackground(new java.awt.Color(0, 0, 51));
         panelSignIn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                panelSignInMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 panelSignInMouseEntered(evt);
             }
@@ -179,12 +193,24 @@ public class mainUI extends javax.swing.JFrame {
         });
         panelSignIn.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel3.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Sign In");
-        panelSignIn.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(28, 5, -1, -1));
+        labelSignIn.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        labelSignIn.setForeground(new java.awt.Color(255, 255, 255));
+        labelSignIn.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelSignIn.setText("Sign In");
+        labelSignIn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                labelSignInMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                labelSignInMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                labelSignInMouseExited(evt);
+            }
+        });
+        panelSignIn.add(labelSignIn, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 100, 30));
 
-        jPanel2.add(panelSignIn, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 100, 30));
+        jPanel2.add(panelSignIn, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 220, 100, 30));
 
         panelNewProfile.setBackground(new java.awt.Color(0, 0, 51));
         panelNewProfile.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -195,13 +221,24 @@ public class mainUI extends javax.swing.JFrame {
                 panelNewProfileMouseExited(evt);
             }
         });
+        panelNewProfile.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("New Profile");
-        panelNewProfile.add(jLabel4);
+        labelNewProfile.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        labelNewProfile.setForeground(new java.awt.Color(255, 255, 255));
+        labelNewProfile.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelNewProfile.setText("New Profile");
+        labelNewProfile.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        labelNewProfile.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                labelNewProfileMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                labelNewProfileMouseExited(evt);
+            }
+        });
+        panelNewProfile.add(labelNewProfile, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 100, 30));
 
-        jPanel2.add(panelNewProfile, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 200, 100, 30));
+        jPanel2.add(panelNewProfile, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 220, 100, 30));
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 0, 260, 300));
 
@@ -230,27 +267,24 @@ public class mainUI extends javax.swing.JFrame {
 
     private void panelSignInMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelSignInMouseEntered
         // changes color of sign in button when hovered
-        panelSignIn.setBackground(new java.awt.Color(0, 0, 120));
+        //panelSignIn.setBackground(new java.awt.Color(0, 0, 120));
     }//GEN-LAST:event_panelSignInMouseEntered
 
     private void panelSignInMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelSignInMouseExited
         // changes color back when mouse stops hovering
-        panelSignIn.setBackground(new java.awt.Color(0, 0, 51));
+        //panelSignIn.setBackground(new java.awt.Color(0, 0, 51));
     }//GEN-LAST:event_panelSignInMouseExited
 
     private void panelNewProfileMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelNewProfileMouseEntered
-        // changes color of new profile button when hovered
-        panelNewProfile.setBackground(new java.awt.Color(0, 0, 120));
+        
     }//GEN-LAST:event_panelNewProfileMouseEntered
 
     private void panelNewProfileMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelNewProfileMouseExited
-        // changes color back when mouse stops hovering
-        panelNewProfile.setBackground(new java.awt.Color(0, 0, 51));
+        
     }//GEN-LAST:event_panelNewProfileMouseExited
 
     private void panelExitMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelExitMouseEntered
-        // changes color of x to white when area is hovered
-        labelExit.setForeground(new java.awt.Color(0,0,0));
+        
     }//GEN-LAST:event_panelExitMouseEntered
 
     private void labelExitMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelExitMouseExited
@@ -275,9 +309,56 @@ public class mainUI extends javax.swing.JFrame {
     }//GEN-LAST:event_labelExitMouseEntered
 
     private void panelExitMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelExitMouseExited
-        // reset exit button to default color
-        labelExit.setForeground(new java.awt.Color(0,0,51));
+        
     }//GEN-LAST:event_panelExitMouseExited
+
+    // USER SIGNS IN HERE
+    private void panelSignInMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelSignInMouseClicked
+        try {
+            // user signs in
+            username = textFieldUserName.getText();
+            password = passwordField.getText();
+            
+            // validation goes here
+            new Dashboard(username).setVisible(true);
+        } catch (IOException ex) {
+            Logger.getLogger(mainUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.setVisible(false);
+    }//GEN-LAST:event_panelSignInMouseClicked
+    // USER ALSO SIGNS IN HERE
+    private void labelSignInMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelSignInMouseClicked
+        try {
+            // user signs in
+            // user signs in
+            username = textFieldUserName.getText();
+            password = passwordField.getText();
+            new Dashboard(username).setVisible(true);
+        } catch (IOException ex) {
+            Logger.getLogger(mainUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.setVisible(false);
+    }//GEN-LAST:event_labelSignInMouseClicked
+
+    private void labelSignInMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelSignInMouseEntered
+        // changes color of sign in button when hovered
+        panelSignIn.setBackground(new java.awt.Color(0, 0, 120));
+    }//GEN-LAST:event_labelSignInMouseEntered
+
+    private void labelSignInMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelSignInMouseExited
+        // changes color back when mouse stops hovering
+        panelSignIn.setBackground(new java.awt.Color(0, 0, 51));
+    }//GEN-LAST:event_labelSignInMouseExited
+
+    private void labelNewProfileMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelNewProfileMouseEntered
+        // changes color of new profile button when hovered
+        panelNewProfile.setBackground(new java.awt.Color(0, 0, 120));
+    }//GEN-LAST:event_labelNewProfileMouseEntered
+
+    private void labelNewProfileMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelNewProfileMouseExited
+        // changes color back when mouse stops hovering
+        panelNewProfile.setBackground(new java.awt.Color(0, 0, 51));
+    }//GEN-LAST:event_labelNewProfileMouseExited
 
     /**
      * @param args the command line arguments
@@ -321,20 +402,20 @@ public class mainUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JLabel labelExit;
     private javax.swing.JLabel labelLogo;
+    private javax.swing.JLabel labelNewProfile;
+    private javax.swing.JLabel labelSignIn;
     private javax.swing.JLabel labelSubtitle;
     private javax.swing.JLabel labelTitle;
     private javax.swing.JPanel panelExit;
     private javax.swing.JPanel panelNewProfile;
     private javax.swing.JPanel panelSignIn;
+    private javax.swing.JPasswordField passwordField;
     private javax.swing.JTextField textFieldUserName;
     // End of variables declaration//GEN-END:variables
 
